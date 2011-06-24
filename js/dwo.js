59b82213f8026c6c6d5f16a096556069c16de835
Andreas Huber <andunix@googlemail.com>
2011-06-24 23:17:53 +0200

    editor textarea can load local cached texts

--- js/dwo.js
+++ js/dwo.js
@@ -1,15 +1,18 @@
 var DEBUG = 1;
 var NAMESPACE = 'note';
+//var XMLRPCURL = 'http://offline.andunix.net/lib/exe/xmlrpc.php';
+var XMLRPCURL = '../../exe/xmlrpc.php';
+var loaded_page = '';
 var service;
 function init() {
 	if (DEBUG) console.log("init() - BEGIN");
-	service = new rpc.ServiceProxy("http://offline.andunix.net/lib/exe/xmlrpc.php", {
+	service = new rpc.ServiceProxy(XMLRPCURL, {
 		asynchronous: false,
 		protocol: "XML-RPC",
 		callbackParamName: 'callback'
 	});
-	if (DEBUG) console.log("DokuWiki version " + service.dokuwiki.getVersion());
-	if (DEBUG) console.log("XML-RPC version " + service.wiki.getRPCVersionSupported());
+	//if (DEBUG) console.log("DokuWiki version " + service.dokuwiki.getVersion());
+	//if (DEBUG) console.log("XML-RPC version " + service.wiki.getRPCVersionSupported());
 	listLocalPages();
 	if (DEBUG) console.log("init() - END");
 }
@@ -44,7 +47,7 @@ function listLocalPages() {
 		var key = localStorage.key(i);
 		if (key.match(/#text$/)) {
 			var page = key.replace(/#text$/, "");
-			pagelist += "<li>"+page+"</li>";
+			pagelist += '<li><a href="javascript:load(\''+page+'\')">'+page+'</a></li>';
 			if (DEBUG) console.log(page);
 		}
 	}
@@ -65,99 +68,16 @@ function listLocalNodes() {
 	$(pagelist).replaceAll('#pagelist');
 	if (DEBUG) console.log("listLocalNodes() - END");
 }
-//
-// EVERYTHING BELOW IS TEST CODE AND CAN BE REMOVED
-//
-function webstorage_test() {
-	var pagelist = "";
-	console.log("WebStorage Test - BEGIN");
-	console.log("PAGES:");
-	for (var i = 0; i < localStorage.length; i++) {
-		var key = localStorage.key(i);
-		if (key.match(/#text$/)) {
-			var page = key.replace(/#text$/, "");
-			console.log(page);
-			pagelist += "<li>"+page+"</li>";
-		}
-	}
-	$(pagelist).replaceAll('#pagelist');
-	console.log("WebStorage Test - END");
+function load(name) {
+	$('#editarea').val(localStorage.getItem(name+"#text"));
+	$('<span id="loaded_page">'+name+'</span>').replaceAll('#loaded_page');
+	loaded_page = name;
 }
-function xmlrpc_test() {
-	console.log("XML-RPC Test - BEGIN");
-	/*
-	var service = new rpc.ServiceProxy("http://offline.andunix.net/lib/exe/xmlrpc.php", {
-		asynchronous: false,
-		protocol: "XML-RPC",
-		sanitize: false,
-		callbackParamName: 'callback'
-	});
-	*/
-	var pages = service.dokuwiki.getPagelist('note', []);
-	console.log(pages);
-	for (var i = 0; i < pages.length; i++) {
-		var page = pages[i];
-		console.log('PAGE: '+page.id);
-		var page_src = service.wiki.getPage(page.id);
-		console.log(page_src);
-	}
-	console.log("XML-RPC Test - END");
+function save() {
+	
 }
-/*
-function xmlrpc_test_1() {
-	console.log("XML-RPC Test - BEGIN");
-	var xml='<?xml version="1.0"?><methodCall><methodName>wiki.getRPCVersionSupported</methodName><params></params></methodCall>';
-	$.ajax({
-		url: "http://offline.andunix.net/lib/exe/xmlrpc.php",
-		data: xml,
-		dataType: "text",
-		contentType:"text/xml",
-		type:"post",
-		success: function(data, textStatus, jqXHR){
-			console.log("XML-RPC Test - SUCCESS: "+data);
-		},
-		error: function(){
-			console.log("XML-RPC Test - ERROR");
-		}
-	});
-	console.log("XML-RPC Test - END");
-}
-*/
-function xmlrpc_download() {
-	console.log("XML-RPC: getAllPages - BEGIN");
-	/*
-	var xml='<?xml version="1.0"?><methodCall><methodName>dokuwiki.getPagelist</methodName><params><param><value><string>note</string></value></param><param><array><data></data></array></param></params></methodCall>';
-	$.ajax({
-		url: "http://offline.andunix.net/lib/exe/xmlrpc.php",
-		data: xml,
-		dataType: "text",
-		contentType:"text/xml",
-		type:"post",
-		success: function(data, textStatus, jqXHR){
-			console.log("XML-RPC: getAllPages - SUCCESS: "+data);
-		},
-		error: function(){
-			console.log("XML-RPC: getAllPages - ERROR");
-		}
-	});
-	*/
-	var pages = service.dokuwiki.getPagelist('note', []);
-	console.log(pages);
-	var pagelist = [];
-	for (var i = 0; i < pages.length; i++) {
-		var page = pages[i];
-		console.log('PAGE: '+page.id);
-		var page_src = service.wiki.getPage(page.id);
-		console.log(page_src);
-		localStorage.setItem(page.id+"#text", page_src);
-		localStorage.setItem(page.id+"#mtime", page.mtime);
-		localStorage.removeItem(page.id+"#text");
-		localStorage.removeItem(page.id+"#mtime");
-		/*
-		pagelist.push(page.id);
-		console.log("pagelist = "+pagelist);
-		localStorage.setItem("pages", pagelist);
-		*/
-	}
-	console.log("XML-RPC: getAllPages - END");
+function empty() {
+	loadedpage = '';
+	$('<span id="loaded_page"></span>').replaceAll('#loaded_page');
+	$('#editarea').val('');
 }
