ae48f629aa8df3d84cca04d66e003c855600eceb
Andreas Huber <andunix@googlemail.com>
2011-07-07 00:18:17 +0200

    fIxed wrong year in copyright

--- js/pagehook.js
+++ js/pagehook.js
@@ -1,3 +1,19 @@
+/*
+ * Copyright 2011 Andreas Huber - http://andunix.net/
+ *
+ * Licensed unter the Creative Commons Attribution 3.0 Germany License
+ * http://creativecommons.org/licenses/by/3.0/de/deed.en
+ *
+ * Lizensiert unter der Creative Commons Namensnennung 3.0 Deutschland Lizenz
+ * http://creativecommons.org/licenses/by/3.0/de/
+ */
+/**
+ * DokuWiki Offline Plugin
+ *
+ * @license Creative Commons Attribution 3.0 Germany License http://creativecommons.org/licenses/by/3.0/de/deed.en
+ * @author Andreas Huber, <a href="http://andunix.net/">http://andunix.net/</a>
+ */
+
 var offline = true;
 
 function set_offline() {
